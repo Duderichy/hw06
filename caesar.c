081b21dd6973a1a3306fb5121d62d81baccf9fb5
Richard Bibeault <Richard@Richards-MacBook-Pro.local>
2016-05-18 21:35:27 -0400

    in progress

---
+++ caesar.c
@@ -0,0 +1,25 @@
+/*
+The purpose of this program is to take an input of a string that is encrypted
+using a caesar cypher and brute force the original message by printing all
+possible original messages
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+void caesar (int key, char message[]);
+
+int main()  { //Take input of the message or have it late in program???
+  int key;
+  // char message = scanf("Enter message to be decyphered: ")
+  char message = "Sp S rkn kcuon zoyzvo grkd droi gkxdon, droi gyevn rkfo cksn pkcdob rybcoc."
+  caesar (int key, char message[])
+}
+
+void caesar (int key, char message[]) {
+  
+}
+
+
+//Write module that that cyphers through once, use main to increment through all
+// in for loop
