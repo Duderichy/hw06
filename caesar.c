4cf6317c2170111aa2d45d5f36328dfc2098e29a
Richard Bibeault <Richard@Richards-MacBook-Pro.local>
2016-05-19 00:36:40 -0400

    works! needs finishing touches

--- caesar.c
+++ caesar.c
@@ -1,6 +1,6 @@
 /*
 The purpose of this program is to take an input of a string that is encrypted
-using a caesar cypher and brute force the original message by printing all
+using a caesar cipher and brute force the original message by printing all
 possible original messages
 */
 
@@ -12,37 +12,41 @@ void caesar (int key, char message[]);
 int main()
 { //Take input of the message or have it late in program???
   int key = 3; // or write in scanf for key (or not)
-  // char message[] = scanf("Enter message to be decyphered: ")
+  // char message[] = scanf("Enter message to be deciphered: ")
   // char message[] = "Sp S rkn kcuon zoyzvo grkd droi gkxdon, droi gyevn rkfo cksn pkcdob rybcoc."
   char message[] = "abcdefghi j k l m n o p q r s t u v w x y z";
-  caesar (key, message[]);
+  caesar (key, message);
 }
 
 void caesar (int key, char message[])
 {
+
   // this function assumes that spaces are the only non alphabet chars in the string
   // and also that all chars are lowercase
-  int i, j;
-  int x = strlen(message[]);
+
+  int i, j, k;
+  int x = strlen(message);
 
   for (j = 0; j < key; j++)  {
     for (i = 0; i < x; i++)  {
       if (message[i] != ' ' && message[i] != 'z') {
-        message[i] = message[i]++;
+        message[i]++;
       }
-      if (message[i] == 'z')  {
+      else  {
+        k = 1;
+      }
+
+      if (message[i] == 'z' && k)  {
         message[i] = 'a';
       }
-      i++;
+      k = 0;
     }
+    printf("%s\n", message);
   }
-
-  printf("%s", message[]);
-
 }
 
 
-//Write module that that cyphers through once, use main to increment through all
+//Write module that that ciphers through once, use main to increment through all
 // in for loop
 // write for loop that iterates for each value of key 0 times for 0 and 1 time
 // for 1 etc
